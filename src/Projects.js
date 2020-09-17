1be4cf8c422b7287dcc9efe0f50c36e240aa1e35
serranomarco <serranomarco@hotmail.com>
2020-09-16 19:56:56 -0700

    resume

--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from "react-pdf";
+import pdfFile from './sample.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 const Projects = () => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        console.log(numPages)
+        setPageNumber(numPages)
+    }
+
     return (
-        <div style={{ backgroundColor: 'black', position: 'absolute', top: '70px', left: '0px', right: '0px', bottom: '0px', display: 'flex' }}>
+        <div style={{ position: 'absolute', top: '70px', left: '0px', right: '0px', bottom: '0px', display: 'flex' }}>
+            <Document onLoadSuccess={onDocumentLoadSuccess} file={pdfFile}>
+                <Page pageNumber={pageNumber} />
+            </Document>
         </div>
     )
 }
